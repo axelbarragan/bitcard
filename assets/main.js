4c370af5ce47115a2a2c1894f9031f2cc15c5495
Axel Barragán <axelmontes92@gmail.com>
2018-12-20 14:31:45 -0600

    Ultimos cambios
    cambios

--- assets/main.js
+++ assets/main.js
@@ -17,43 +17,7 @@ $(document).ready(function() {
 		}
 	});
 
-	$("form[name='registration']").validate({
-    // Specify validation rules
-    rules: {
-      // The key name on the left side is the name attribute
-      // of an input field. Validation rules are defined
-      // on the right side
-      firstname: "required",
-      lastname: "required",
-      email: {
-        required: true,
-        // Specify that email should be validated
-        // by the built-in "email" rule
-        email: true
-      },
-      password: {
-        required: true,
-        minlength: 5
-      }
-    },
-    // Specify validation error messages
-    messages: {
-      firstname: "Please enter your firstname",
-      lastname: "Please enter your lastname",
-      password: {
-        required: "Please provide a password",
-        minlength: "Your password must be at least 5 characters long"
-      },
-      email: "Please enter a valid email address"
-    },
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
-    submitHandler: function(form) {
-      form.submit();
-    }
-  });
-
-	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	//Scroll
 $(function() {
 	$(document).on('click', 'a.page-scroll', function(event) {
 		var $anchor = $(this);
@@ -63,7 +27,6 @@ $(function() {
 		event.preventDefault();
   });
 });
-
 
 
 });
