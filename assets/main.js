d8ec02c4162fcfefec98d83eaefc6365e016800f
Axel Barragán <axelmontes92@gmail.com>
2018-12-27 19:09:37 -0600

    Cambios boton
    Cambios boton

--- assets/main.js
+++ assets/main.js
@@ -21,9 +21,13 @@ $(document).ready(function() {
 $(function() {
 	$(document).on('click', 'a.page-scroll', function(event) {
 		var $anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top-25
-			}, 1200, 'easeInOutExpo');
+		var idTop = "#top";
+		if($(this).attr('href') == idTop) {
+			var valor = 100;
+		} else {
+			var valor = 25;
+		}
+		$('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top-valor}, 1200, 'easeInOutExpo');
 		event.preventDefault();
   });
 });
